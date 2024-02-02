f6489d5d49c18d00a8cf9a3a3283ae05d935c75a
Riccardo Genova <r.genova@bitrocket.dev>
2024-02-02 15:25:53 +0100

    feat: add base node architecture

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+fs_1.default.writeFileSync('example.txt', 'TEST');
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = 3000;
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.get('/api', (req, res) => {
+    res.json({ message: "Benvenuto nell'API!" });
+});
+app.listen(PORT, () => {
+    console.log(`Server run: localhost:${PORT}`);
+});
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.get('/api', (req: Request, res: Response) => {
+  res.json({ message: "Benvenuto nell'API!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server run: localhost:${PORT}`);
+});
